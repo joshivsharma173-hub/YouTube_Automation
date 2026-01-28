2227ddc36a10b1dcfa0ed09a00fe1268376c73bb
Joshiv Sharma <joshivsharma173@gmail.com>
2026-01-28 19:48:19 +0530

    ME_QA_XYOUTUBE_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -10,20 +10,23 @@ import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+import static demo.wrappers.Wrappers.*;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.logging.Level;
 
 import demo.utils.ExcelDataProvider;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
-public class TestCases extends ExcelDataProvider{ // Lets us read the data
+public class TestCases extends ExcelDataProvider { // Lets us read the data
         ChromeDriver driver;
 
         /*
@@ -62,6 +65,186 @@ public class TestCases extends ExcelDataProvider{ // Lets us read the data
         public void endTest() {
                 driver.close();
                 driver.quit();
+
+        }
+
+        @BeforeMethod
+        public void navigateToYouTube() {
+                Wrappers wrappers = new Wrappers(driver);
+                wrappers.navigateToUrl("https://www.youtube.com/");
+
+        }
+
+        @Test
+        public void testCase01() throws InterruptedException {
+                System.out.println("=============TestCase01 Starts=============");
+                Wrappers wrapper = new Wrappers(driver);
+                Boolean status = driver.getCurrentUrl().contains("youtube");
+                Assert.assertTrue(status, "URL does not contains youtube ");
+                WebElement sideButton = driver.findElement(By.xpath("//button[@aria-label='Guide']"));
+                WebElement about = driver.findElement(By.xpath("//a[text()='About']"));
+                pause(3000);
+                if (!about.isDisplayed()) {
+                        wrapper.click(sideButton);
+
+                }
+                wrapper.click(about);
+
+                WebElement aboutText = driver.findElement(By.tagName("h1"));
+                wrapper.printText(aboutText);
+                // //a[text()='About']
+                List<WebElement> list = driver.findElements(By.tagName("p"));
+                wrapper.printAbout(list);
+                System.out.println("=============TestCase01 Ends===============");
+
+        }
+
+        @Test
+        public void testCase02() throws InterruptedException {
+                System.out.println("=============TestCase02 Starts=============");
+                Wrappers wrapper = new Wrappers(driver);
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+                WebElement sideButton = driver.findElement(By.xpath("//button[@aria-label='Guide']"));
+                List<WebElement> moviesList = driver.findElements(By.xpath("//a[@title='Movies']"));
+                pause(3000);
+                if (moviesList.isEmpty()) {
+                        wrapper.click(sideButton);
+                }
+                WebElement movies = wait
+                                .until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@title='Movies']")));
+                wrapper.click(movies);
+
+                wait.until(ExpectedConditions.urlContains("storefront"));
+
+                // //button[@aria-label='Next']
+                WebElement next = wait.until(
+                                ExpectedConditions.elementToBeClickable(By.xpath("//button[@aria-label='Next']")));
+
+                while (next.isDisplayed()) {
+                        wrapper.click(next);
+
+                }
+
+                List<WebElement> list = driver.findElements(By.xpath("//div[@id='items']//ytd-grid-movie-renderer"));
+
+                List<String> movieList = wrapper.getMovieTypeAndAge(list);
+                String actualMovieType = movieList.get(0);
+                String actualAge = movieList.get(1);
+
+                SoftAssert softAssert = new SoftAssert();
+                softAssert.assertTrue(actualMovieType.equals("Comedy") || actualMovieType.equals("Drama")
+                                || actualMovieType.equals("Animation"), "Movie Type is not matching");
+
+                softAssert.assertTrue(actualAge.equals("A") || actualAge.equals("U/A")
+                                || actualAge.equals("U"), "Movie Type is not matching");
+
+                softAssert.assertAll();
+                // //div[@id='items']//ytd-grid-movie-renderer//span[@id='video-title'] ====>
+                // Video title
+                // //div[@id='items']//ytd-grid-movie-renderer//span[contains(@class,'renderer-metadata')]
+                // ===> Type of movie
+                // //div[@id='items']//ytd-grid-movie-renderer//*[contains(@class,'badges
+                // style')]//div[2]//div ====> U or U/A
+
+                System.out.println("=============TestCase02 Ends===============");
+
+        }
+
+        @Test
+        public void testCase03() throws InterruptedException {
+                System.out.println("=============TestCase03 Starts=============");
+                Wrappers wrapper = new Wrappers(driver);
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+                WebElement sideButton = driver.findElement(By.xpath("//button[@aria-label='Guide']"));
+                pause(3000);
+                List<WebElement> musicList = driver.findElements(By.xpath("//a[@title='Music']"));
+                if (musicList.isEmpty()) {
+                        wrapper.click(sideButton);
+                }
+                WebElement music = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@title='Music']")));
+                wrapper.click(music);
+
+                wait.until(ExpectedConditions.urlContains("channel"));
+
+                WebElement parentEle = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(
+                                "(//ytd-rich-shelf-renderer)[1]//ytd-rich-item-renderer[not(@hidden)][last()]")));
+                WebElement childSongNum = parentEle.findElement(By.xpath(".//div[@class='yt-badge-shape__text']"));
+
+                WebElement childSongTitle = parentEle.findElement(By.xpath(".//h3//span"));
+                String numText = childSongNum.getText().trim().split(" ")[0];
+                String titleText = childSongTitle.getText();
+                int num = Integer.parseInt(numText);
+                System.out.println(num + "====>" + titleText);
+
+                SoftAssert softAssert = new SoftAssert();
+                softAssert.assertTrue(num <= 60, "Songs number are not less than or equals 50");
+                softAssert.assertAll();
+
+                // (//ytd-rich-shelf-renderer)[1]//ytd-rich-item-renderer[not(@hidden)][last()]
+                // =====> last right most non-hidden
+                // (//ytd-rich-shelf-renderer)[1]//ytd-rich-item-renderer[not(@hidden)][last()]//div[@class='yt-badge-shape__text']
+                // =====> Title
+                // (//ytd-rich-shelf-renderer)[1]//ytd-rich-item-renderer[not(@hidden)][last()]//h3//span
+                System.out.println("=============TestCase03 Ends===============");
+        }
+
+        @Test
+        public void testCase04() throws InterruptedException {
+                System.out.println("=============TestCase04 Starts=============");
+                Wrappers wrapper = new Wrappers(driver);
+                WebElement sideButton = driver.findElement(By.xpath("//button[@aria-label='Guide']"));
+
+                // //a[@title='Movies']
+                List<WebElement> newsList = driver.findElements(By.xpath("//a[@title='News']"));
+                pause(3000);
+                if (newsList.isEmpty()) {
+                        wrapper.click(sideButton);
+
+                }
+                By showMoreBy = By.xpath("//yt-formatted-string[text()='Show more']");
+
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+                List<WebElement> showMoreList = driver.findElements(showMoreBy);
+
+                if (!showMoreList.isEmpty()) {
+                        WebElement showMore = wait.until(ExpectedConditions.elementToBeClickable(showMoreBy));
+                        showMore.click();
+                }
+                System.out.println(showMoreList);
+                // if (driver.findElement(By.xpath("//yt-formatted-string[text()='Show
+                // more']")).isDisplayed()) {
+                // driver.findElement(By.xpath("//yt-formatted-string[text()='Show
+                // more']")).click();
+
+                // }
+
+                WebElement news = wait.until(ExpectedConditions.elementToBeClickable(
+                                By.xpath("//a[@title='News']")));
+                wrapper.click(news);
+
+                wait.until(ExpectedConditions.urlContains("channel"));
+
+                List<WebElement> list = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath(
+                                "//div[@id='rich-shelf-header-container' and contains(.,'Latest news posts')]//ancestor::div[1]//div[@id='contents']//ytd-rich-item-renderer[not(@hidden)]")));
+
+                pause(3000);
+
+                double sum = wrapper.getNewsHeadingAndSum(list);
+                System.out.println("Total sum of Likes Count is " + "====>" + sum);
+
+                System.out.println(list.size());
+
+                // //div[@id='rich-shelf-header-container' and contains(.,'Latest news
+                // posts')]//ancestor::div[1]//div[@id='contents']//ytd-rich-item-renderer[not(@hidden)]
+                // ====> List of News
+                // .//div[@id='author'] ======> author
+                // .//div[@id='post-text']//yt-formatted-string[@id='home-content-text']//span[1]
+                // ======> body
+                // .//span[@id='vote-count-middle'] =====> like count
+
+                System.out.println("=============TestCase04 Ends===============");
 
         }
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -6,12 +6,120 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.asserts.SoftAssert;
+
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    ChromeDriver driver;
+    WebDriverWait wait;
+
+    public Wrappers(ChromeDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public boolean navigateToUrl(String text) {
+        driver.get(text);
+        Boolean status = wait.until(ExpectedConditions.urlContains("youtube"));
+        return status;
+
+    }
+
+    public void click(WebElement ele) {
+        JavascriptExecutor js = driver;
+        js.executeScript("arguments[0].click()", ele);
+    }
+
+    public static void pause(long n) throws InterruptedException {
+        Thread.sleep(n);
+    }
+    public void printText(WebElement ele){
+        String text = ele.getText();
+                System.out.println(text);
+    }
+
+    public void printAbout(List<WebElement> list){
+        for (WebElement ele : list) {
+                        String text = ele.getText();
+                        System.out.println(text);
+                }
+
+    }
+
+    public List<String> getMovieTypeAndAge(List<WebElement> list)
+            throws InterruptedException {
+        List<String> movieList = new ArrayList<>();
+        WebElement lastMovie = list.get(list.size() - 1);
+        pause(3000);
+
+        WebElement movieType = lastMovie.findElement(By.xpath(".//span[contains(@class,'renderer-metadata')]"));
+        WebElement ageType = lastMovie.findElement(By.xpath("*[contains(@class,'badges style')]//div[2]//div"));
+
+        String typeText1 = movieType.getText().trim();
+        String typeText = typeText1.split(" ")[0];
+        String ageText = ageType.getText().trim();
+
+        System.out.println(list.size());
+        System.out.println(typeText + "====>" + ageText);
+
+        movieList.add(typeText);
+        movieList.add(ageText);
+
+        return movieList;
+    }
+
+    public double getNewsHeadingAndSum(List<WebElement> list){
+        double num =0;
+        for(WebElement parentEle : list){
+                        String author = parentEle.findElement(By.xpath(".//div[@id='author']")).getText().trim();
+                        List<WebElement> elems = parentEle.findElements(By.xpath(".//div[@id='post-text']//yt-formatted-string[@id='home-content-text']//span[1]"));
+                        String body = elems.isEmpty() ? "" : elems.get(0).getText().trim();
+                        //String body = parentEle.findElement(By.xpath(".//div[@id='post-text']//yt-formatted-string[@id='home-content-text']//span[1]")).getText().trim();
+                        String likeCount= parentEle.findElement(By.xpath(".//span[@id='vote-count-middle']")).getText().trim();
+                        System.out.println(author +"====>" + body + "====>" + likeCount);
+                        num = num + numericalValue(likeCount);
+                        
+    }
+    return num;
+
+}
+
+    public double numericalValue(String likeCount){
+
+         if(!likeCount.isEmpty()){
+                                likeCount = likeCount.toUpperCase();
+                                char lastLetter = likeCount.charAt(likeCount.length()-1);
+                                int multiplier = 1;
+                                switch(lastLetter){
+                                        case 'K':
+                                                multiplier = 1*1000;
+                                                break;
+                                        case 'M':
+                                                multiplier = 1*1000000;
+                                                break;
+                                        case 'B':
+                                                multiplier = 1*100000000;
+                                                break;
+                                        default :
+                                                if(Character.isDigit(lastLetter)){
+                                                        return Double.parseDouble(likeCount);
+                                                }
+                                }
+                                String text = likeCount.substring(0, likeCount.length()-1); // 100K --> 100
+                                double num= Double.parseDouble(text);
+                                return num = num*multiplier;
+                        }
+        return 0;
+
+
+    }
 }
